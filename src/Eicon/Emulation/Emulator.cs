a3d18e39174ba1631fccfd53d558a972c2601702
Vivian <43400068+vPyxi@users.noreply.github.com>
2019-05-18 02:15:44 -0500

    Add Emulator.cs

---
+++ src/Eicon/Emulation/Emulator.cs
@@ -0,0 +1,18 @@
+using Eicon.Graphics;
+using Eicon.Graphics.Implementations;
+
+namespace Eicon.Emulation
+{
+    public class Emulator
+    {
+        private IRenderer _renderer = new CGraph();
+
+        public void Bootstrap(string filepath) {
+            
+        }
+
+        public void Run() {
+
+        }
+    }
+}
