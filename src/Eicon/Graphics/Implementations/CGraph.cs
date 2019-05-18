3e1b7a0efd7737223c186ec09f0c4f6f1760f03e
Vivian <43400068+vPyxi@users.noreply.github.com>
2019-05-18 02:15:12 -0500

    Add CGraph.cs, basic IRenderer implementation

---
+++ src/Eicon/Graphics/Implementations/CGraph.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Eicon.Graphics.Implementations
+{
+    public class CGraph : IRenderer
+    {
+        public void Initialize() {
+            Console.Clear();
+        }
+
+        public void Deinitialize() {
+
+        }
+
+        public void ClearBuffer() {
+
+        }
+
+        public void RenderFrame() {
+
+        }
+
+        public void SetPixel(int x, int y, byte[] color) {
+
+        }
+
+        public void PushChanges() {
+
+        }
+    }
+}
